95c4820573402689d45630204d8605f692c59231
Shannon Skipper <shannonskipper@gmail.com>
2013-02-07 22:27:36 -0800

    altitude variation for the roflcopter

--- roflcopter.rb
+++ roflcopter.rb
@@ -5,11 +5,13 @@ include Curses
 
 init_screen
 curs_set(frame = 0)
+altitude = 5
 
 Thread.new { system "say -v Alex the rofl copter says #{'siff ' * 1000}" }
 
 while (frame += 1)
   [
+    %|                               |,
     %|      4321043210LOL4321043210  |,
     %|                 ^             |,
     %|012     /-------------         |,
@@ -17,22 +19,29 @@ while (frame += 1)
     %|210       \\            \\       |,
     %|           \\____________]      |,
     %|              I     I          |,
-    %|          --------------/      |
+    %|          --------------/      |,
+    %|                               |,
   ].each_with_index do |line, index|
-    { 0 => [':LFOR', 5], (2..4) => ['L    ', 4] }.each do |key, (ch, n)|
+    { 1 => [':LFOR', 5], (3..5) => ['L    ', 4] }.each do |key, (ch, n)|
       n.times { |i| line.tr!(i.to_s, ch.chars.to_a[(i + frame) % n]) } if key === index
     end
 
     copter = (line + (' ' * (cols - 31))).chars.to_a
     frame.times { copter.unshift(copter.pop) }
-
-    setpos(5 + index, 0)
+    
+    setpos(altitude + index, 0)
     addstr copter.join
   end
 
+  case rand 1..10
+  when 1
+    altitude -= 1
+  when 10
+    altitude += 1
+  end
+  
   refresh
   sleep 0.08
 end
 
 at_exit { close_screen }
-
