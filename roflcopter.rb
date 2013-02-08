7dd1eeee2c6e305fec09eb565cf5c0418fb86a0a
Gino Lucero <glucero@gmail.com>
2013-02-07 19:47:21 -0700

    change alignment

--- roflcopter.rb
+++ roflcopter.rb
@@ -10,21 +10,21 @@ Thread.new { system "say -v Alex the rofl copter says #{'siff ' * 1000}" }
 
 while (frame += 1)
   [
-    %|       4321043210LOL4321043210 |,
+    %|      4321043210LOL4321043210  |,
     %|                 ^             |,
-    %|012      /-------------        |,
+    %|012     /-------------         |,
     %|3O3=======        [ ] \\        |,
-    %|210      \\            \\        |,
-    %|         \\____________]        |,
-    %|            I     I            |,
-    %|        --------------/        |
+    %|210       \\            \\       |,
+    %|           \\____________]      |,
+    %|              I     I          |,
+    %|          --------------/      |
   ].each_with_index do |line, index|
     { 0 => [':LFOR', 5], (2..4) => ['L    ', 4] }.each do |key, (ch, n)|
       n.times { |i| line.tr!(i.to_s, ch.chars.to_a[(i + frame) % n]) } if key === index
     end
 
     copter = (line + (' ' * (cols - 31))).chars.to_a
-    (index + frame).times { copter.unshift(copter.pop) }
+    frame.times { copter.unshift(copter.pop) }
 
     setpos(5 + index, 0)
     addstr copter.join
